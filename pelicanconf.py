95932f3d05b8b612621c7d228888dee714eaecc1
Jason Hamilton-Smith <hs.jason@gmail.com>
2016-02-01 21:49:13 -0800

    Initial Commit

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jason Hamilton-Smith'
+SITENAME = u'Zchfvy'
+SITEURL = ''
+
+PATH = 'content'
+
+THEME = 'alchemy'
+
+TIMEZONE = 'America/Vancouver'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+LICENSE_NAME = 'Creative Commons Attribution-ShareAlike 4.0 International License.'
+LICENSE_URL = 'http://creativecommons.org/licenses/by-sa/4.0/'
+
+EMAIL_ADDRESS = 'hs.jason@gmail.com'
+TWITTER_ADDRESS = 'https://twitter.com/jason_hs'
+GITHUB_ADDRESS = 'https://github.com/jason-hs'
